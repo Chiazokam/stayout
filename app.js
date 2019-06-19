5c3c2218b01c0da72d1def9125ec779ac8d7efd8
Chiazokam Echeta <chiazokamecheta@gmail.com>
2019-06-19 10:43:36 +0100

    166470623-task(deployment): deploy to heroku
    - create pipeline for apps
    - modify docs file to a .ymal file
    - create review app
    - push to heroku
    
    [Finishes #166470623]

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
-import docs from './docs.json';
+import YAML from 'yamljs';
 import router from './server/routes';
 
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+const apiDocs = YAML.load(path.join(process.cwd(), './docs.yml'));
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(docs));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
 
-app.use('/api/v1', router);
+app.use('/', router);
 
 app.listen(process.env.PORT || 5000, () => {
   // eslint-disable-next-line no-console
