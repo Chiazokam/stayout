c045ca4fdd11964064728a8b01835c6e92de2efe
Chiazokam Echeta <chiazokamecheta@gmail.com>
2019-07-04 15:09:12 +0100

    166997281-story(auth): setup jwt authentication
    - setup jwt
    - write function to generate token
    - generate documentation for signup
    
    [Delivers #166997281]

--- app.js
+++ app.js
@@ -2,23 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
 import router from './server/routes';
 
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-const apiDocs = YAML.load(path.join(process.cwd(), './docs.yml'));
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/json' }));
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
 
 app.use('/api/v1', router);
 
